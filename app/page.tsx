7cd8134d1650f7e6e48548cbec90c0b279755d3d
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-08-10 11:32:07 -0700

    Remove Supabase connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,3 @@
-import Link from "next/link";
-import Form from "./Form";
-
-async function getPosts() {
-  const res = await fetch(`${process.env.BASE_URL}/api/getPosts`);
-  if (!res.ok) {
-    console.log("Error", res);
-  }
-  return res.json();
-}
-export default async function Home() {
-  const data: { id: number; title: string }[] = await getPosts();
-
-  return (
-    <main className="py-7 px-48">
-      <Link
-        href="/dashboard"
-        className="bg-teal-700 text-black font-medium py-2 px-4 rounded-md"
-      >
-        Go to the dashboard
-      </Link>
-      {data.map((post) => (
-        <h1>{post.title}</h1>
-      ))}
-      <Form />
-    </main>
-  );
+export default function Home() {
+  return <main className="py-7 px-48">Blog!</main>;
 }
