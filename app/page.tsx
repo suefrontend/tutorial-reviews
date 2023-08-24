e503b2a71713940d0d67fd2ec6552c872652b79a
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-08-23 20:57:46 -0700

    Delete files

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-import ArticleList from "./components/ArticleList";
-
 export default async function Home() {
-  return (
-    <main className="py-7 px-48">
-      This is my blog
-      <ArticleList />
-    </main>
-  );
+  return <div className="Home"></div>;
 }
