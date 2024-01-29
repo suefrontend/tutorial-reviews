57e7a065a5befd570c00e10b145530eaed1ba644
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2024-01-28 22:05:10 -0800

    Display article title from Contentful

--- app/components/ArticleItem.tsx
+++ app/components/ArticleItem.tsx
@@ -1,46 +1,57 @@
 import Image from "next/image";
 
-function ArticleItem() {
-  return (
-    <article className="border p-6">
-      <div className="border-b">
-        <h3 className="text-blue text-lg font-medium -mb-1">
-          Advanced CSS and Sass: Flexbox, Grid, Animations and More!
-        </h3>
-        <div className="flex items-center py-4">
-          <ul className="flex mr-2 gap-2">
-            <li className="flex items-center text-[12px] text-lightgray font-light py-[2px] px-2 bg-bggray">
-              CSS
-            </li>
-            <li className="flex items-center text-[12px] text-lightgray font-light py-[2px] px-2 bg-bggray">
-              Tailwind CSS
-            </li>
-          </ul>
-          <p className="font-light text-sm text-lightgray">
-            Advanced · Paid · 2018 · 70:25:39
+function ArticleItem(props) {
+  const { blog } = props;
+
+  console.log("blog", blog);
+
+  return blog.items.map((item) => {
+    const { title } = item.fields;
+
+    return (
+      <article className="border p-6">
+        <div className="border-b">
+          <h3 className="text-blue text-lg font-medium -mb-1">
+            {title}
+          </h3>
+          <div className="flex items-center py-4">
+            <ul className="flex mr-2 gap-2">
+              <li className="flex items-center text-[12px] text-lightgray font-light py-[2px] px-2 bg-bggray">
+                CSS
+              </li>
+              <li className="flex items-center text-[12px] text-lightgray font-light py-[2px] px-2 bg-bggray">
+                Tailwind CSS
+              </li>
+            </ul>
+            <p className="font-light text-sm text-lightgray">
+              Advanced · Paid · 2018 · 70:25:39
+            </p>
+          </div>
+        </div>
+
+        <div className="flex gap-6 pt-4">
+          {/* <div className="border w-1/3"> */}
+          <div
+            className="relative w-1/5 h-auto"
+            style={{ aspectRatio: "16/9" }}
+          >
+            <Image
+              src="/images/css-jonas.jpeg"
+              alt="image"
+              fill
+              className="object-cover"
+            />
+          </div>
+          {/* </div> */}
+          <p className="font-light w-4/5 leading-relaxed -mt-1">
+            Lorem ipsum dolor sit amet consectetur. Bibendum enim ornare
+            sagittis molestie. Curabitur iaculis fermentum volutpat ut turpis
+            feugiat sit pellentesque. At aliquam adipiscing.
           </p>
         </div>
-      </div>
-
-      <div className="flex gap-6 pt-4">
-        {/* <div className="border w-1/3"> */}
-        <div className="relative w-1/5 h-auto" style={{ aspectRatio: "16/9" }}>
-          <Image
-            src="/images/css-jonas.jpeg"
-            alt="image"
-            fill
-            className="object-cover"
-          />
-        </div>
-        {/* </div> */}
-        <p className="font-light w-4/5 leading-relaxed -mt-1">
-          Lorem ipsum dolor sit amet consectetur. Bibendum enim ornare sagittis
-          molestie. Curabitur iaculis fermentum volutpat ut turpis feugiat sit
-          pellentesque. At aliquam adipiscing.
-        </p>
-      </div>
-    </article>
-  );
+      </article>
+    );
+  });
 }
 
 export default ArticleItem;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
+import Link from "next/link";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
+import { createClient } from "contentful";
+
+const client = createClient({
+  space: process.env.SPACE_ID,
+  accessToken: process.env.ACCESS_TOKEN,
+});
+
+const getBlogEntries = async () => {
+  const entries = await client.getEntries({ content_type: "blog" });
+  console.log("entries", entries)
+  return entries;
+};
 
 export default async function Home() {
+  const blogEntries = await getBlogEntries();
+
   return (
     <div>
       <Header />
@@ -11,7 +26,7 @@ export default async function Home() {
         <Hero />
         <div className="flex gap-10">
           <Sidebar />
-          <Main />
+          <Main blog={blogEntries} />
         </div>
       </div>
     </div>
